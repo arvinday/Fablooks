e1155d8e8540e640ad3b6baf3f90899cada6e5bf
Arvind <arvinday@gmail.com>
2022-03-03 17:45:06 +0800

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "info@fablooks.co.in";
+$recipient = "arvinday@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
